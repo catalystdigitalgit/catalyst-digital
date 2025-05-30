1a4b5e5a50c5f911cff1f09fa7331795a58a7179
catalystdigitalgit <contact@catalystdigital.uk>
2025-05-30 17:49:06 +0100

    Updated ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -63,6 +63,7 @@ export default function ContactPage() {
                   </div>
                 </div>
               </div>
+              </div>
               </section>
 
             {/* Contact Form */}
