ad1824742e773ac40c938b531438eb27e48d5eea
catalystdigitalgit <contact@catalystdigital.uk>
2025-05-30 16:25:38 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,7 +76,7 @@ export default {
           light: 'hsl(var(--accent-light))',
           dark: 'hsl(var(--accent-dark))',
           foreground: 'hsl(var(--accent-foreground))',
-          blue: '#231493',
+          blue: '#140871',
         },
         
         destructive: {
